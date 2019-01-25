29267268b64b10228a972de9d4d5ea036419d607
Long Ma <long6@cs.ualberta.ca>
2019-01-25 13:32:49 -0700

    changed license info

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socketserver
 import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Long Ma
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,35 +23,44 @@ import os
 #
 # http://docs.python.org/2/library/socketserver.html
 #
-# run: python freetests.py
-
-# try: curl -v -X GET http://127.0.0.1:8080/
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
     def sort_request(self):
+        #extracts requested file and method of the http request
         request_split = self.data.split(' ')
+        #gets the method and file of the request
         method = request_split[0][2:]
         requested_file = request_split[1]
         self.requested_path=requested_file
-    
+
+
+        #returns True if it can be handled by our server, false if it can't
         if method=="GET":
             return True
         else:
             return False
 
     def check_path(self):
+        #Checks the path requested and if the file exists or not and returns
+        #different numbers depending on what file is requested
+        
+        #cannot go up in directory
         if '/../' in self.requested_path:
             return 0
         elif os.path.isfile('./www'+self.requested_path):
+            #if the file exist return 1
             self.requested_type=self.requested_path.split('.')[1]
             return 1
         
         elif os.path.isdir('./www'+self.requested_path[:-1]) and self.requested_path[-1:]=="/":
+            #if requesting a directory return 2
             return 2
         elif os.path.isdir('./www'+self.requested_path):
+            #if moved then return 3
             return 3
         else:
+            #if the requested path does nto exist return 0
             return 0
 
         return True
@@ -62,23 +71,40 @@ class MyWebServer(socketserver.BaseRequestHandler):
             self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed",'utf-8'))
         requested_status=self.check_path()
         if requested_status==1:
+            #handles a requested file
+            
             f=open('www'+self.requested_path)
             data=f.read()
+            #returns a 200 ok
             status="HTTP/1.1 200 OK\r\nContent-Type: text/{}\r\nContent-Length:{}\r\n\r\n".format(self.requested_type,str(len(data)))
             self.request.sendall(bytearray(status,'utf-8'))
+            #sends the file requested
             self.request.sendall(bytearray(data,'utf-8'))
             
             self.request.close()
+
+            
         elif requested_status==2:
+            #if requested a directory
+            
             f=open('www'+self.requested_path+'index.html')
             data=f.read()
+            #returns a 200 ok
             self.request.sendall(bytearray("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length:{} \r\n\r\n".format(str(len(data))),'utf-8'))
             self.request.sendall(bytearray(data,'utf-8'))
+            #return the index page
             self.request.close()
+
+            
         elif requested_status==3:
+            #if requesting a directory without / at the end
+            #returns a 301
             self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently\r\nLocation:{}/\r\n\r\n".format(self.requested_path),'utf-8'))
             self.request.close()
+
+            
         elif requested_status==0:
+            #if the requested file/directory does not exist, return a 404
             self.request.sendall(bytearray("HTTP/1.1 404 Not Found",'utf-8'))
             self.request.close()
             
